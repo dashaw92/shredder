793210033bf8fff1db8bd79ffee92a9490407173
dashaw92 <dashaw92@gmail.com>
2019-02-17 15:51:09 -0500

    Added an icon

--- src/me/daniel/shredder/ShredderApplication.java
+++ src/me/daniel/shredder/ShredderApplication.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ShredderApplication extends Application {
@@ -24,6 +25,7 @@ public class ShredderApplication extends Application {
 	public void start(Stage stage) throws Exception {
 		Parent root = FXMLLoader.load(getClass().getResource("shredder.fxml"));
 		
+		stage.getIcons().add(new Image(ShredderApplication.class.getResourceAsStream("shredder.png")));
 		stage.setTitle("File Shredder");
 		stage.setScene(new Scene(root, 549, 530));
 		stage.setResizable(false);
